a3cf75a158f4478096ceeecd6325f20ceda3afd7
maxefimov <efimov.top.academy@gmail.com>
2023-12-22 20:36:27 +0300

    Добавьте файлы проекта.

---
+++ CppFuncsVarsProject/CppFuncsVarsProject.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int Sqr(int n)
+{
+    return n * n;
+}
+
+int Counter()
+{
+    static int count{ 0 };
+    return ++count;
+}
+
+int Func()
+{
+    int n{ 0 };
+    return ++n;
+}
+
+int main()
+{
+    for (int i{}; i < 5; i++)
+    {
+        std::cout << Counter() << "\n";
+        std::cout << Func() << "\n";
+    }
+
+}
